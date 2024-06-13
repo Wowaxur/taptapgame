4958f6873659c18da5b1b47702c0210eac9769d0
Wowaxur <wowaxur@yandex.ru>
2024-06-13 03:28:57 +0300

    add coin, score and energy bar, and boosters

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import Coin from "./components/coin/Coin";
+import {useDispatch, useSelector} from "react-redux";
+import {
+    decrementEnergy,
+    incrementScore,
+    recoverEnergy,
+    RootState,
+    upgradeMaxEnergyLevel,
+    upgradeRecoveryLevel,
+    upgradeTapLevel
+} from "./store/store";
+import EnergyBar from "./components/energyBar/energyBar";
+import ScoreBar from "./components/scoreBar/ScoreBar";
+import UpgradeBar from "./components/upgradeBar/UpgradeBar";
+import {TapValue} from "./store/LvlSystem";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { score, currentEnergy, currentMaxEnergy } = useSelector((state: RootState) => state);
+    const dispatch = useDispatch();
+    const tapLVL = useSelector((state: RootState) => state.tapLVL);
+    const tapValue = TapValue[tapLVL];
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            dispatch(recoverEnergy());
+        }, 10000);
+        return () => clearInterval(interval);
+    }, [dispatch]);
+
+    const coinClick = () => {
+        if (currentEnergy >= tapValue) {
+            dispatch(incrementScore());
+            dispatch(decrementEnergy());
+        } else {
+            console.log("Not enough energy to click the coin.");
+        }
+    };
+
+
+    const upgradeClickLVL = () => {
+        dispatch(upgradeTapLevel());
+    };
+    const upgradeRecoveryLVL = () => {
+        dispatch(upgradeRecoveryLevel());
+    }
+    const upgradeMaxEnergyLVL = () => {
+        dispatch(upgradeMaxEnergyLevel());
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header"></header>
+            <div className={'container'}>
+                <ScoreBar score={score}/>
+                <Coin tapValue={tapValue} currentEnergy={currentEnergy} onClick={coinClick}/>
+                <br/>
+                <EnergyBar
+                    maxEnergy={currentMaxEnergy}
+                    currentEnergy={currentEnergy}
+                />
+                <UpgradeBar
+                    upgradeMaxEnergyLevel={upgradeMaxEnergyLVL}
+                    upgradeRecoveryLevel={upgradeRecoveryLVL}
+                    upgradeClickLVL={upgradeClickLVL}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default App;
